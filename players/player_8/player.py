86963b0f5f205e29056edcbecef8bc6ec519eef6
soumilbaldota <ssb2234@columbia.edu>
2025-09-15 11:54:12 -0400

    fix: respond to comment to modify init

--- players/player_8/player.py
+++ players/player_8/player.py
@@ -2,7 +2,7 @@ from models.player import Item, Player, PlayerSnapshot
 
 
 class Player8(Player):
-	def __init__(self, snapshot: PlayerSnapshot, conversation_length: int) -> None:
+	def __init__(self, snapshot: PlayerSnapshot, conversation_length: int) -> None:  # noqa: F821
 		super().__init__(snapshot, conversation_length)
 
 	@staticmethod
