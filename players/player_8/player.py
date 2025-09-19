b2a43c9540f2a8ee4fa37e29720d1f1faf438bee
soumilbaldota <ssb2234@columbia.edu>
2025-09-19 11:57:52 -0400

    feat: tune weights for the bonus function

--- players/player_8/player.py
+++ players/player_8/player.py
@@ -1,9 +1,11 @@
 from collections import Counter
+import sys
 from uuid import UUID
 from models.player import GameContext, Item, Player, PlayerSnapshot
 
+v = [3.509549936829426, 3.6661804644288427, 2.657774084696876, 4.354161981052706, 2.4502569274937183, -1.386507441304629]
 
-class Player8Base(Player):
+class Player8(Player):
 	def __init__(self, snapshot: PlayerSnapshot, ctx: GameContext) -> None:  # noqa: F821
 		snapshot.item_in_memory_bank
 		super().__init__(snapshot, ctx)
@@ -33,7 +35,7 @@ class Player8Base(Player):
 		if not history:
 			return 1 / number_of_players
 
-		items_contributed = Player8Base.compute_player_counts(history)
+		items_contributed = Player8.compute_player_counts(history)
 		if not items_contributed:
 			return 1 / number_of_players
 
@@ -57,7 +59,6 @@ class Player8Base(Player):
 	def compute_bonuses(
 		item: Item, history: list[Item], monotonic_subjects: list[int], preferences: list[int]
 	) -> list[float]:
-
 		if not item:  # pause shortcut
 			return [1, 0, 0, 0, 0, 0]
 
@@ -65,36 +66,36 @@ class Player8Base(Player):
 			return -2 if item in history else 0
 
 		def preference_bonus():
-			return Player8Base.preference_score(preferences, item)
+			return Player8.preference_score(preferences, item)
 
 		def importance_bonus():
 			return item.importance
 
 		def freshness_bonus():
-			if not Player8Base.was_last_round_pause(history):
+			if not Player8.was_last_round_pause(history):
 				return 0
-			prev_subjects = Player8Base.get_last_n_subjects(history, 6)
+			prev_subjects = Player8.get_last_n_subjects(history, 6)
 			for s in item.subjects:
 				if s not in prev_subjects:
 					return 1
 			return 0
 
 		def coherence_bonus():
-			context_subjects = set(Player8Base.current_context(history))
-			return Player8Base.coherence_score(item, context_subjects)
+			context_subjects = set(Player8.current_context(history))
+			return Player8.coherence_score(item, context_subjects)
 
 		def monotonic_bonus():
 			if not item:
 				return 0
 			return sum(-1 for s in item.subjects if s in monotonic_subjects)
-
+		
 		return [
-			freshness_bonus() * 0.8,
-			coherence_bonus() * 1.2,
-			monotonic_bonus() * 5,
-			repetition_bonus() * 5,
-			importance_bonus() * 0.5,
-			preference_bonus() * 0.5,
+			freshness_bonus() * v[0],
+			coherence_bonus() * v[1],
+			monotonic_bonus() * v[2],
+			repetition_bonus() * v[3],
+			importance_bonus() * v[4],
+			preference_bonus() * v[5],
 		]
 
 	@staticmethod
@@ -112,14 +113,13 @@ class Player8Base(Player):
 		if not items:
 			return None
 		return max(items, key=lambda item: item.importance)
-	
 
 	@staticmethod
 	def monotonic_subjects(history: list[Item]) -> list[int]:
 		monotonic_subjects = []
 
 		if len(history) >= 3:
-			counter = Counter(Player8Base.subjects_from_items(history[-3:]))
+			counter = Counter(Player8.subjects_from_items(history[-3:]))
 			for x, y in counter.items():
 				if y > 2:
 					monotonic_subjects.append(x)
@@ -128,10 +128,13 @@ class Player8Base(Player):
 	@staticmethod
 	def coherence_score(item: Item, context_subjects: list[Item]):
 		coherence_score = -1
-		for subject in item.subjects:
-			item_has_current_subject = subject in context_subjects
-			if item_has_current_subject:
-				coherence_score += 1
+
+		if len(item.subjects) == 1:
+			coherence_score += (item.subjects[0] in context_subjects) * 2
+		if len(item.subjects) == 2:
+			coherence_score += item.subjects[0] in context_subjects
+			coherence_score += item.subjects[1] in context_subjects
+
 		return coherence_score
 
 	@staticmethod
@@ -153,15 +156,14 @@ class Player8Base(Player):
 		)
 
 	@staticmethod
-	def get_first_unused_item( items: list[Item], history: list[Item]) -> Item | None:
-		return next(iter(Player8Base.filter_unused(items, history)), None)
+	def get_first_unused_item(items: list[Item], history: list[Item]) -> Item | None:
+		return next(iter(Player8.filter_unused(items, history)), None)
 
 	@staticmethod
 	def filter_monotonic_items(monotonic_subjects: list[int], items: list[Item]) -> list[Item]:
 		return [item for item in items if not len(set(monotonic_subjects) & set(item.subjects))]
 
 	def propose_item(self, history: list[Item]) -> Item | None:
-
 		if None not in self.memory_bank:
 			self.memory_bank.append(None)
 
@@ -175,4 +177,4 @@ class Player8Base(Player):
 
 		best_item, best_bonus = max(evaluated_items, key=lambda x: x[1])
 
-		return best_item if best_bonus >= 0.0 else None
+		return best_item if best_bonus >= 0 else None
